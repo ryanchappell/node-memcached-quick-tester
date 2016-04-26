c397b7b329cbf5f726740899b2314d1939883702
ryanchappell <ryan.p.chap@gmail.com>
2016-04-26 13:40:02 -0700

    making tests sequential

--- app.js
+++ app.js
@@ -1,45 +1,82 @@
+var Promise = require('es6-promise').Promise;
 var Memcached = require('memcached');
 var memcached = new Memcached("192.168.6.25:11211");
 
 console.log('starting up!');
 
-console.log('testing touch');
-memcached.touch('ryan-test-key', 1000, function(err, ok){
-  if (err){
-    console.log('touch error: ' + err);
-  } else {
-    console.log('touch result: ' + ok);
-  }
-});
 
-console.log('testing get');
-memcached.get('ryan-test-key', function(err, ok){
-  if (err){
-    console.log('get error: ' + err);
-  } else {
-    console.log('get result: ' + ok);
-  }
-});
-//
-console.log('testing set');
-memcached.set('ryan-test-key', 'ryan-test-value', 1000, function(err, ok){
-  if (err)
-  {
-    console.log('set error: ' + err);
-  } else {
-    console.log('set result: ' + ok);
-  }
+function testTouch(){
+  return new Promise(function(resolve, reject){
+    console.log('# testing touch');
+    memcached.touch('ryan-test-key', 1000, function(err, ok){
+      if (err){
+        console.log('touch error: ' + err);
+        reject(err);
+      } else {
+        console.log('touch result: ' + ok);
+        resolve(ok);
+      }
+    });
+  });
+}
 
-});
-//
-console.log('testing delete');
-memcached.delete('ryan-test-key', function(err, ok){
-  if (err)
-  {
-    console.log('delete error: ' + err);
-  } else {
-    console.log('delete result: ' + ok);
-  }
-});
+function testGet()
+{
+  return new Promise(function(resolve, reject){
+    console.log('# testing get');
+    memcached.get('ryan-test-key', function(err, ok){
+      if (err){
+        console.log('get error: ' + err);
+        reject(err);
+      } else {
+        console.log('get result: ' + ok);
+        resolve(ok);
+      }
+    });
+  });
+}
 
-console.log('done!');
+function testSet()
+{
+  return new Promise(function(resolve, reject){
+    console.log('# testing set');
+    memcached.set('ryan-test-key', 'ryan-test-value', 1000, function(err, ok){
+      if (err)
+      {
+        console.log('set error: ' + err);
+        reject(err);
+      } else {
+        console.log('set result: ' + ok);
+        resolve(ok);
+      }
+    });
+  });
+}
+
+function testDelete(){
+  return new Promise(function(resolve, reject){
+    console.log('# testing delete');
+    memcached.delete('ryan-test-key', function(err, ok){
+      if (err)
+      {
+        console.log('delete error: ' + err);
+        reject(err);
+      } else {
+        console.log('delete result: ' + ok);
+        resolve(ok);
+      }
+    });
+  });
+}
+
+testSet()
+.then(testTouch)
+.then(testGet)
+.then(testDelete)
+.then(testGet)
+.then(function(result){
+  console.log('done!');
+})
+.catch(function(error){
+  console.log('error occurred: ' + error);
+});
